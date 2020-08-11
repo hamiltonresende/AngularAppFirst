2cf73713a7e8710a59378f5b78e549876024e268
Hamilton Ferreira <hamilton.rferreira@gmail.com>
2020-08-11 19:31:15 -0300

    Incrementando listas com ngFor.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server created!';
   serverName = "TestServer";
   serverCreated = false;
+  servers = ['TestServer', 'TestServer 2']
 
   constructor() { 
     setTimeout(() => {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
